5b7b3cdb5cb27f8243e0b3a1ede7e31144de4948
user <user@PC523>
2021-02-01 10:45:05 +0100

    Funcinalidad basica

--- src/main/java/es/programia/info/web/GestionSolicitudesManagedBean.java
+++ src/main/java/es/programia/info/web/GestionSolicitudesManagedBean.java
@@ -5,9 +5,11 @@
  */
 package es.programia.info.web;
 
+import es.programia.excepciones.GestionSolicitudesException;
 import es.programia.info.entidades.Interesado;
 import es.programia.info.servicios.InteresadosServiceLocal;
 import java.io.Serializable;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
@@ -24,6 +26,11 @@ public class GestionSolicitudesManagedBean implements Serializable {
 
     private Interesado interesado;
     
+    private boolean editable;
+    private boolean visible;
+    private boolean accionModificar;
+    private boolean accionNuevo;
+    
     @EJB
     private InteresadosServiceLocal servicio;
     
@@ -37,6 +44,40 @@ public class GestionSolicitudesManagedBean implements Serializable {
         this.interesado = interesado;
     }
 
+    public boolean isEditable() {
+        return editable;
+    }
+
+    public void setEditable(boolean editable) {
+        this.editable = editable;
+    }
+
+    public boolean isVisible() {
+        return visible;
+    }
+
+    public void setVisible(boolean visible) {
+        this.visible = visible;
+    }
+
+    public boolean isAccionModificar() {
+        return accionModificar;
+    }
+
+    public void setAccionModificar(boolean accionModificar) {
+        this.accionModificar = accionModificar;
+    }
+
+    public boolean isAccionNuevo() {
+        return accionNuevo;
+    }
+
+    public void setAccionNuevo(boolean accionNuevo) {
+        this.accionNuevo = accionNuevo;
+    }
+    
+    
+
     public static Logger getLog() {
         return log;
     }
@@ -48,9 +89,52 @@ public class GestionSolicitudesManagedBean implements Serializable {
     
     
     public GestionSolicitudesManagedBean() {
+        
+    }
+    @PostConstruct
+    public void inicializar(){
         interesado=new Interesado();
+        editable=false;
+        visible=false;
+        accionModificar=true;
+        accionNuevo=true;
     }
     
-
-    
+    //Acciones
+    public void buscar(){
+        try {
+            Interesado interesadoBusc=new Interesado();
+            interesadoBusc=servicio.buscarInteresadoPorCriterio(this.interesado.getNombre(), this.interesado.getApellidos() , this.interesado.getEmpresa());
+            setEditable(false);
+            setVisible(true);
+            // Guardo el interesado buscado dentro de la variable interesado
+            interesado=interesadoBusc;
+            
+        } catch (GestionSolicitudesException ex) {
+            Logger.getLogger(GestionSolicitudesManagedBean.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    public void modoNuevo(){
+        try {
+            Interesado interesadoBusc=new Interesado();
+            interesadoBusc=servicio.buscarInteresadoPorCriterio(this.interesado.getNombre(), this.interesado.getApellidos() , this.interesado.getEmpresa());
+            setEditable(true);
+            setVisible(true);
+            // Guardo el interesado buscado dentro de la variable interesado
+            interesado=interesadoBusc;
+            
+        } catch (GestionSolicitudesException ex) {
+            Logger.getLogger(GestionSolicitudesManagedBean.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    public void cancelar(){
+        setEditable(false);
+        setVisible(false);
+    }
+    public void grabar(){
+        
+    }
+    public void editar(){
+        
+    }
 }
