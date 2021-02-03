0563e40e13a73f5a120db3043ce97563b41bcc59
user <user@PC523>
2021-02-03 09:16:55 +0100

    Modificiar los metodos serviceLocal añadiendo las etiquetas para que no falle cascade

--- src/main/java/es/programia/info/servicios/InteresadosServiceLocal.java
+++ src/main/java/es/programia/info/servicios/InteresadosServiceLocal.java
@@ -8,10 +8,20 @@ package es.programia.info.servicios;
 import es.programia.excepciones.GestionSolicitudesException;
 import es.programia.info.entidades.*;
 import javax.ejb.Local;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import javax.ejb.TransactionManagement;
+import javax.ejb.TransactionManagementType;
 
 @Local
+@TransactionManagement(TransactionManagementType.CONTAINER)
 public interface InteresadosServiceLocal {
+    @TransactionAttribute(TransactionAttributeType.SUPPORTS)
     public Interesado buscarInteresadoPorCriterio(String nombre, String apellido, String empresa) throws GestionSolicitudesException;
+    
+    @TransactionAttribute(TransactionAttributeType.REQUIRED)
     void CrearNuevoInteresado(Interesado interesado) throws GestionSolicitudesException;
+    
+    @TransactionAttribute(TransactionAttributeType.REQUIRED)
     void modificarInteresado(Interesado interesado) throws GestionSolicitudesException;
 }
